24ff323840704d490f978face0d681c38ca40a85
Sheldon <0xd34d10cc@gmail.com>
2018-09-14 00:02:37 +0000

    Add benchmark for compression

--- src/encoder.rs
+++ src/encoder.rs
@@ -24,9 +24,7 @@ impl Encoder for EncoderGrayscale {
         let levels = metadata.scale_level;
         let mut positions = PositionMap::new(width, height);
         let mut predictions = Vec::<PredictMap>::new();
-
-        let depth = grid.len() - 1;
-        predictions.resize(depth + 1 as usize, PredictMap::new());
+        predictions.resize(levels + 1 as usize, PredictMap::new());
 
         let level = 0;
         let step = 1 << levels;
@@ -40,58 +38,45 @@ impl Encoder for EncoderGrayscale {
             }
         }
 
-        let encode_pixel = |column: u32, line: u32, previous_level: &PredictMap, input: &GrayImage| -> (u8, u8) {
-            let (post_inter_value, predicted_value) = {
-                let prediction = get_interp_pixels(
-                    depth,
-                    level,
-                    (width, height),
-                    (column, line),
-                    previous_level,
-                    0,
-                ).prediction();
-
-                let pix_value = input.get_pixel(column, line).data[0];
-                let post_inter_value = (pix_value as i32 - prediction as i32).abs() as u8;
-                (post_inter_value, prediction)
-            };
-            let quanted_postinter_value = metadata.quantizator.quantize(post_inter_value);
-            (quanted_postinter_value, predicted_value)
-        };
-
         for level in 0..levels {
             let e = levels - level;
             let start = 1 << (e - 1);
             let step = 1 << e;
             let substep = start;
 
+            let encode_pixel = |column: u32,
+                                line: u32,
+                                previous_level: &PredictMap,
+                                input: &GrayImage|
+             -> (u8, u8) {
+                let (post_inter_value, predicted_value) = {
+                    let prediction = get_interp_pixels(
+                        levels,
+                        level + 1,
+                        (width, height),
+                        (column, line),
+                        previous_level,
+                        0,
+                    ).prediction();
+
+                    let pix_value = input.get_pixel(column, line).data[0];
+                    let post_inter_value = (pix_value as i32 - prediction as i32).abs() as u8;
+                    (post_inter_value, prediction)
+                };
+                let quanted_postinter_value = metadata.quantizator.quantize(post_inter_value);
+                (quanted_postinter_value, predicted_value)
+            };
+
             let mut column = 0;
             while column < width {
                 for line in (start..height).step_by(step) {
-                    let (post_inter_value, predicted_value) = {
-                        let mut previous_level = &predictions[level];
-
-                        let prediction = get_interp_pixels(
-                            depth,
-                            level + 1,
-                            (width, height),
-                            (column, line),
-                            previous_level,
-                            0,
-                        ).prediction();
-
-                        let pix_value = input.get_pixel(column, line).data[0];
-                        let post_inter_value = (pix_value as i32 - prediction as i32).abs() as u8;
-                        (post_inter_value, prediction)
-                    };
-                    let quanted_postinter_value = metadata.quantizator.quantize(post_inter_value);
+                    let (quanted_postinter_value, predicted_value) = encode_pixel(column, line, &predictions[level], &input);
 
                     let pixel = gray(quanted_postinter_value.saturating_add(predicted_value));
                     input.put_pixel(column, line, pixel);
                     grid[level + 1].push(quanted_postinter_value);
                     predictions[level + 1]
                         .insert((column as usize, line as usize), quanted_postinter_value);
-
                 }
 
                 column += substep;
@@ -100,80 +85,16 @@ impl Encoder for EncoderGrayscale {
                 }
 
                 for line in (0..height).step_by(substep as usize) {
-                    let (post_inter_value, predicted_value) = {
-                        let mut previous_level = &predictions[level];
-
-                        let prediction = get_interp_pixels(
-                            depth,
-                            level + 1,
-                            (width, height),
-                            (column, line),
-                            previous_level,
-                            0,
-                        ).prediction();
-
-                        let pix_value = input.get_pixel(column, line).data[0];
-                        let post_inter_value = (pix_value as i32 - prediction as i32).abs() as u8;
-                        (post_inter_value, prediction)
-                    };
-                    let quanted_postinter_value = metadata.quantizator.quantize(post_inter_value);
-
+                    let (quanted_postinter_value, predicted_value) = encode_pixel(column, line, &predictions[level], &input);
                     let pixel = gray(quanted_postinter_value.saturating_add(predicted_value));
                     input.put_pixel(column, line, pixel);
                     grid[level + 1].push(quanted_postinter_value);
                     predictions[level + 1]
                         .insert((column as usize, line as usize), quanted_postinter_value);
-
                 }
                 column += substep;
             }
         }
         return grid;
-    }
-}
-
-//////////////////
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-    use decoder::{Decoder, DecoderGrayscale};
-    use image;
-    use utils::{Interpolator, Quantizator};
-
-    #[test]
-    fn losseless_compression_test() {
-        let metadata = Metadata {
-            quantizator: Quantizator::Loseless,
-            interpolator: Interpolator::Crossed,
-            dimensions: (8, 8),
-            scale_level: 2,
-        };
-
-        let mut imgbuf = image::ImageBuffer::new(8, 8);
-
-        // fill image with random pixels
-        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-            *pixel = image::Luma([(x * y) as u8]);
-        }
-
-        for line in imgbuf.chunks(imgbuf.width() as usize) {
-            println!("{:?}", line);
-        }
-
-        let mut encoder = EncoderGrayscale {};
-        let grid = encoder.encode(&metadata, imgbuf.clone());
-
-        let mut decoder = DecoderGrayscale {};
-        let image = decoder.decode(&metadata, &grid);
-
-        for line in image.chunks(image.width() as usize) {
-            println!("{:?}", line);
-        }
-
-        for (x, y, pixel) in imgbuf.enumerate_pixels() {
-            assert!((pixel.data[0] as i32 - image[(x, y)].data[0] as i32).abs() < 10);
-        }
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+extern crate bincode;
+extern crate bit_vec;
+extern crate byteorder;
+extern crate flate2;
+extern crate image;
+extern crate serde;
+#[macro_use]
+extern crate clap;
+
+#[macro_use]
+extern crate serde_derive;
+
+mod utils;
+mod encoder;
+mod archive;
+mod decoder;
+
+pub use self::utils::{Metadata, Quantizator, Interpolator};
+pub use self::encoder::{Encoder, EncoderGrayscale};
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use decoder::{Decoder, DecoderGrayscale};
+    use image;
+    use utils::{Interpolator, Quantizator};
+
+    type Pixel = image::Luma<u8>;
+    type Subpixel = <Pixel as image::Pixel>::Subpixel;
+    type Container = Vec<Subpixel>;
+    type GrayscaleBuffer = image::ImageBuffer<Pixel, Container>;
+
+    fn get_test_image(width: u32, height: u32, levels: usize) -> (Metadata, GrayscaleBuffer) {
+        let metadata = Metadata {
+            quantizator: Quantizator::Loseless,
+            interpolator: Interpolator::Crossed,
+            dimensions: (width, height),
+            scale_level: levels,
+        };
+
+        let mut imgbuf = image::ImageBuffer::new(width, height);
+
+        // fill image with random pixels
+        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+            *pixel = image::Luma([(x * y) as u8]);
+        }
+
+        (metadata, imgbuf)
+    }
+
+    #[test]
+    fn losseless_compression_test() {
+        let (metadata, imgbuf) = get_test_image(8, 8, 3);
+
+        for line in imgbuf.chunks(imgbuf.width() as usize) {
+            println!("{:?}", line);
+        }
+
+        let mut encoder = EncoderGrayscale {};
+        let grid = encoder.encode(&metadata, imgbuf.clone());
+
+        let mut decoder = DecoderGrayscale {};
+        let image = decoder.decode(&metadata, &grid);
+
+        for line in image.chunks(image.width() as usize) {
+            println!("{:?}", line);
+        }
+
+        for (x, y, pixel) in imgbuf.enumerate_pixels() {
+            let before = pixel.data[0] as i32;
+            let after = image[(x, y)].data[0] as i32;
+
+            assert!((before - after).abs() < 10);
+        }
+    }
+
+}
